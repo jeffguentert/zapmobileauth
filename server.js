d4cb49fdc01dd751c8b51d3bb745ce2c433b15b3
Jeff Guentert <jguentert@ziprealty.com>
2017-09-02 17:41:58 -0700

    =

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('/', function (req, res, next) {
         "&response_type=code"+
         "&response_mode=query"+
         "&scope=openid+phone+email+profile+groups"+
-        "&redirect_uri=https://zapmobileauth.herokuapp.com"+
+        "&redirect_uri=https://zapmobileauth.herokuapp.com/"+
         "&state=staticState&nonce=staticNonce",
         method: 'GET',
     };
