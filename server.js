e92fba83583885954adba2498f22cac91314e109
Jeff Guentert <jguentert@ziprealty.com>
2017-09-04 09:01:31 -0700

    =update redirect

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(function(req, res, next) {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
       //res.header('Access-Control-Allow-Credentials', true);
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With','uuid','zap_mobile_session');
       res.send(200);
     }
     else {
