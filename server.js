38483f7d914b2c7f9ec9fdc9784d66d7b0404ec9
Jeff Guentert <jguentert@ziprealty.com>
2017-09-04 09:35:23 -0700

    =cors

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ const port = process.env.PORT || 3000;
 //app.use(cors({credentials: true, origin: true}));
 
 app.use(function(req, res, next) {
-    //if ('OPTIONS' == req.method) {
+    if ('OPTIONS' == req.method) {
       res.header('Access-Control-Allow-Origin', 'http://localhost');
       res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, HEAD');
       res.header('Access-Control-Allow-Credentials', true);
       res.header('Content-Type','X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers,zap_mobile_session,uuid');
       res.send(200);
-  //  }
-  //  else {
-///      next();
-  //  }
+    }
+   else {
+      next();
+  }
 });
 
 app.get('/index.html', function (req, res, next) {
