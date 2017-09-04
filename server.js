372bd53bceafedf5b5c668639427b149bbd00790
Jeff Guentert <jguentert@ziprealty.com>
2017-09-04 10:10:35 -0700

    =cors

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const port = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  //next();
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, uuid, zap_mobile_session");
+  next();
 });
 app.get('/index.html', function (req, res, next) {
   let getSessionToken = function(){
