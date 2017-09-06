afcad47d0d09d165b6c919c553087d8172d37b6b
Jeff Guentert <jguentert@ziprealty.com>
2017-09-06 14:47:38 -0700

    working version returns idtoken

--- server.js
+++ server.js
@@ -3,57 +3,47 @@ const app = express();
 const rp = require('request-promise');
 const request = require('request');
 const port = process.env.PORT || 3000;
-const oid = process.env.okta_cid || "123";
-require('request-debug')(rp);
+const oid = process.env.okta_cid || "0oabwlejtqISPrMij0h7";
+//require('request-debug')(rp);
 app.use(function(req, res, next) {
-  console.log( req.root);
   res.header("Access-Control-Allow-Origin", "http://localhost");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, uuid, zap_mobile_session");
   next();
 });
 
 app.get('/index.html', function (req, res, next) {
-console.log('in index', req.query);
-
-    let getSessionToken = function() {
-      let options = {
-          uri: 'https://dev-505299-admin.oktapreview.com/api/v1/authn',
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            'username': req.query.username,
-            'password': req.query.password,
-            'options': {
-              "multiOptionalFactorEnroll": false,
-              "warnBeforePasswordExpired": false
-            }
-          })
-      }
-      rp(options).then(function (a) {
-         //res.write( response);
-         let respJson = JSON.parse(a);
-         //res.send(respJson.sessionToken);
-         getAuthCode(respJson.sessionToken);
-         console.log('session r', respJson.sessionToken);
-         //
-      })
-      .catch(function (err) {
-        // Deal with the error
-        returnResponseToClient(err);
-      })
+  let getSessionToken = function() {
+    let options = {
+        uri: 'https://dev-505299.oktapreview.com/api/v1/authn',
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          'username': req.query.username,
+          'password': req.query.password,
+          'options': {
+            "multiOptionalFactorEnroll": false,
+            "warnBeforePasswordExpired": false
+          }
+        })
     }
-    getSessionToken();
-
-  //openIdUrl ="https://" + orgUrl + "/oauth2/v1/authorize?sessionToken="+sessionToken+"
-  //&client_id="+clientId+"&scope=openid+phone+email+profile+groups&response_type=" + tokenType + "
-  //&response_mode=fragment&nonce=staticNonce&redirect_uri="+redirectUri+"&state=staticState"
+    rp(options).then(function (a) {
+       let respJson = JSON.parse(a);
+       //res.send(respJson.sessionToken);
+       getAuthCode(respJson.sessionToken);
+    })
+    .catch(function (err) {
+      // Deal with the error
+      returnResponseToClient(err);
+    })
+  }
+  getSessionToken();
 
   let getAuthCode = function (st) {
     let options = {
-        uri: "https://dev-505299-admin.oktapreview.com/oauth2/v1/authorize",
+        uri: "https://dev-505299.oktapreview.com/oauth2/v1/authorize",
         method: "GET",
         qs: {
             client_id: oid,
@@ -61,7 +51,7 @@ console.log('in index', req.query);
             response_type: "id_token",
             response_mode: "fragment",
             scope: "openid",
-            redirect_uri: "http://localhost:3000/index.html",
+            redirect_uri: "https://zapmobileauth.herokuapp.com/index.html",
             nonce: "static-once",
             state: "static-state"
         },
@@ -69,19 +59,22 @@ console.log('in index', req.query);
         resolveWithFullResponse: true,
         followOriginalHttpMethod: false,
         removeRefererHeader: false,
+        useQuerystring: true,
         simple: false //handle promise other than 200
-
     };
 
     rp(options).then(function (a) {
-       returnResponseToClient(a);
+      var start = a.headers.location.indexOf("#id_token=") + 10;
+      var end = a.headers.location.indexOf("&state=") - 42;
+      var result = a.headers.location.substr(start,end)
+      console.log(result);
+      returnResponseToClient(result);
       // Handle the response
     })
     .catch(function (err) {
       // Deal with the error
       returnResponseToClient(err);
     })
-
   }
 
   function returnResponseToClient(r) {
