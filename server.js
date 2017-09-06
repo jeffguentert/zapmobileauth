2c548dbd88c247a24834bcca0ac7dfd010ee9740
Jeff Guentert <jguentert@ziprealty.com>
2017-09-05 20:11:47 -0700

    testing request options

--- server.js
+++ server.js
@@ -13,11 +13,8 @@ app.use(function(req, res, next) {
 });
 
 app.get('/index.html', function (req, res, next) {
-//  let respJson = JSON.parse(res);
-//  if(respJson.sessionToken) {
-//    getAuthCode(respJson.sessionToken);
-//  }
-console.log('bod', res);
+console.log('in index', req.query);
+
     let getSessionToken = function() {
       let options = {
           uri: 'https://dev-505299-admin.oktapreview.com/api/v1/authn',
@@ -49,17 +46,10 @@ console.log('bod', res);
       })
     }
     getSessionToken();
-//  }
-  //else {
-//    res.send('other');
-//  }
 
   //openIdUrl ="https://" + orgUrl + "/oauth2/v1/authorize?sessionToken="+sessionToken+"
   //&client_id="+clientId+"&scope=openid+phone+email+profile+groups&response_type=" + tokenType + "
   //&response_mode=fragment&nonce=staticNonce&redirect_uri="+redirectUri+"&state=staticState"
-
-
-
 
   let getAuthCode = function (st) {
     let options = {
@@ -85,33 +75,19 @@ console.log('bod', res);
         simple: false //handle promise other than 200
         */
     };
-
-function callback(error, response, body) {
-  console.log('call', error, response, body);
-  returnResponseToClient(error + body + response.statusCode);
-  //  console.log('error:', error); // Print the error if one occurred
-  //  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //  console.log('body:', body); // Print the HTML for the Google homepage.
+    console.log(options);
+    rp(options).then(function (a) {
+       returnResponseToClient(a);
+      // Handle the response
+    })
+    .catch(function (err) {
+      // Deal with the error
+      returnResponseToClient(err);
+    })
   }
-
-  request(options, callback)
-  /*
-      console.log(options);
-      rp(options).then(function (a) {
-         returnResponseToClient(a);
-        // Handle the response
-      })
-      .catch(function (err) {
-        // Deal with the error
-        returnResponseToClient(err);
-      })
-      */
-  }
-
   function returnResponseToClient(r) {
     res.send(r);
   }
-
 });
 
 app.listen(port);
